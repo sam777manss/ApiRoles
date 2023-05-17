daae68af889ed07b3233b8829f7e1f500ac02e9c
user.email <sam777manss@gmail.com>
2023-05-17 22:42:48 +0530

    Add project files.

---
+++ ApiRoles/Auth/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiRoles.Auth
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ ApiRoles/Auth/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiRoles.Auth
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
